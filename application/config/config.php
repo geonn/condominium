c039b052c0bb5158f0a060185b55270521ed5e1e
lauyikonn <lau.yikonn@gmail.com>
2014-08-03 16:08:26 +0800

    timezone added

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
+date_default_timezone_set('asia/kuala_lumpur');
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
